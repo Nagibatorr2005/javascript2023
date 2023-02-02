9a8755f56c909e568038e2ab881873bde4f7e943
Nagibatorr2005 <sgrin7525@gmail.com>
2023-02-02 09:48:09 +0200

    Домашя робота js1

---
+++ homework1/homework1.js
@@ -0,0 +1,69 @@
+/*
+ЗАВДАННЯ №1
+let a = 'hello';
+console.log(a);
+
+let b = 'owu';
+console.log(b);
+
+let c = 'com';
+console.log(c);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let f = 10;
+console.log(f);
+
+let g = -999;
+console.log(g)
+
+let h = 123;
+console.log(h);
+
+let i = 3.14;
+console.log(i);
+
+let j = 2.7;
+console.log(j)
+
+let k = 16;
+console.log(k)
+
+let l = true;
+console.log(l);
+
+let m = false;
+console.log(m);*/
+
+
+/*
+ЗАВДАННЯ №2
+let firstName = 'Святослав', middleName = 'Олегович', lastName = 'Гринь';
+let person;
+person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);*/
+
+
+/*
+ЗАВДАННЯ №3
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);*/
+
+
+/*
+ ДОДАТКОВЕ ЗАВДАННЯ
+let a = prompt('ENTER YOUR NAME');
+let b = prompt('ENTER YOUR MIDDLENAME');
+let c = prompt('ENTER YOUR AGE');
+
+console.log(`Вік:${c} Ім'я:${a} По-Батькові:${b}`)*/
